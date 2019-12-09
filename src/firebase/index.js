3d21c75f03dfa6e2a7f1afa28e44fc7e16359cf5
nhoang53 <he_llo1002@yahoo.com>
2019-12-08 16:22:16 -0800

    upload

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,14 +4,7 @@ import 'firebase/storage';
 // Initialize Firebase
 var config = {
     /* your firebase config*/
-    apiKey: "AIzaSyCnMwujRshSEADYuUnxc2S79nmF63o2DYY",
-    authDomain: "food-fighter.firebaseapp.com",
-    databaseURL: "https://food-fighter.firebaseio.com",
-    projectId: "food-fighter",
-    storageBucket: "food-fighter.appspot.com",
-    messagingSenderId: "700541497420",
-    appId: "1:700541497420:web:6c0e2c6e0293102c7044e9",
-    measurementId: "G-6PYZ9PVXTS"
+    
 };
 firebase.initializeApp(config);
 
